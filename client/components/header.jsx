4a9eb46a3463dba8e387ed930163d89d287234f4
Jenna Bowman <63511719+JBowman1@users.noreply.github.com>
2020-07-20 10:53:30 -0700

    Update header.jsx

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,6 +4,6 @@ export default function Header(props) {
   return (
   <h1>Student Grade Table</h1>
   <h3>{props.title}</h3>
-  <h4> {props.averageTitle} <span>{props.averageGrade}</span></h4>
+  <h4>{props.averageTitle} <span>{props.averageValue}</span></h4>
   );
 }
