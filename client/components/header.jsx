e4c01fa12fce0cea3e5c046ece94bcfef8d04989
Jenna Bowman <jbowman1@swarthmore.edu>
2020-07-20 23:30:45 +0000

    Completed user can delete grade

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,8 +4,8 @@ export default function Header(props) {
   return (
     <div className='col'>
       <h1>Student Grade Table</h1>
-      <h3>{props.title}</h3>
-      <h4>{props.averageTitle} <span>{props.averageValue}</span></h4>
+      <h4>{props.title}</h4>
+      <h4 className="average">{props.averageTitle} <span className="badge badge-secondary">{props.averageValue}</span></h4>
     </div>
   );
 }
